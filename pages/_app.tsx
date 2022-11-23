ac7958e4a353df162e6b65ca84a843e2d9bdbd18
Mark Linn <marktlinn@gmail.com>
2022-11-23 18:30:39 +0100

    trial: to fix issue with bootstrap styles changing on deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/globals.css"
 import { Overpass } from "@next/font/google"
@@ -11,6 +12,9 @@ import Footer from '../components/Footer'
 
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
 
   return (
     <SSRProvider>
